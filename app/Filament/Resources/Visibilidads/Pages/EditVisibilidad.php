7a4d8ece5b04ffa2372b6f48f55c4e6e25591d73
Amancayjesus136 <amancayjesus136@gmail.com>
2026-01-11 13:46:32 -0500

    modification visiibilidad

--- app/Filament/Resources/Visibilidads/Pages/EditVisibilidad.php
+++ app/Filament/Resources/Visibilidads/Pages/EditVisibilidad.php
@@ -11,46 +11,55 @@ class EditVisibilidad extends EditRecord
 {
     protected static string $resource = VisibilidadResource::class;
 
+    protected function getSavedNotificationTitle(): ?string
+    {
+        return 'Visibilidad actualizada correctamente';
+    }
+
     protected function getHeaderActions(): array
     {
         return [
-
             Action::make('archivar')
                 ->label('Archivar')
                 ->icon('heroicon-o-archive-box')
-                ->color('warning')
+                ->extraAttributes([
+                    'class' => 'bg-yellow-500 text-white hover:bg-yellow-600 focus:ring-yellow-500 border-none [&>svg]:!text-white',
+                    'style' => 'background-color: #f49200 !important; color: white !important;',
+                ])
                 ->requiresConfirmation()
                 ->visible(fn () => $this->record->estado_visibilidad === 1)
                 ->action(function () {
                     $this->record->update([
                         'estado_visibilidad' => 0,
                     ]);
-
-                    return redirect(
-                        static::$resource::getUrl('index')
-                    );
+                    return redirect(static::$resource::getUrl('index'));
                 })
-                ->successNotificationTitle('Registro archivado'),
+                ->successNotificationTitle('Registro archivado correctamente'),
 
             Action::make('activar')
                 ->label('Activar')
                 ->icon('heroicon-o-check-circle')
-                ->color('success')
+                ->extraAttributes([
+                    'class' => 'bg-green-600 text-white hover:bg-green-700 focus:ring-green-500 border-none [&>svg]:!text-white',
+                    'style' => 'background-color: #16a34a !important; color: white !important;',
+                ])
                 ->requiresConfirmation()
                 ->visible(fn () => $this->record->estado_visibilidad === 0)
                 ->action(function () {
                     $this->record->update([
                         'estado_visibilidad' => 1,
                     ]);
-
-                    return redirect(
-                        static::$resource::getUrl('index')
-                    );
+                    return redirect(static::$resource::getUrl('index'));
                 })
-                ->successNotificationTitle('Registro activado'),
+                ->successNotificationTitle('Registro activado correctamente'),
 
             DeleteAction::make()
                 ->icon('heroicon-o-trash'),
         ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return static::$resource::getUrl('index');
     }
 }
